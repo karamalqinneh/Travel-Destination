4e98d3041541001595824052e6a141f4c9f492a9
karamalqinneh <karamalqinneh@live.com>
2022-02-10 22:32:29 +0200

    added the basic structure

---
+++ travel-destination/src/App.js
@@ -0,0 +1,7 @@
+import Home from "./components/home/Home";
+
+function App() {
+  return <Home></Home>;
+}
+
+export default App;
---
+++ travel-destination/src/components/footer/Footer.js
@@ -0,0 +1,9 @@
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>A Porject by Karam Al-Qinneh</footer>
+  );
+};
+
+export default Footer;
